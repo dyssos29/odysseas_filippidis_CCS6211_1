05753ad09a260c1d22443baef41d5a64dcc99ebf
dyssos29 <o.filippidis@gmail.com>
2026-04-07 07:11:26 +0300

    feat(js): make js modular and implement the fetching data functionality

---
+++ js/utility.js
@@ -0,0 +1,32 @@
+export const makeHttpRequest = async (url, options = null) => {
+  try {
+    const response = await fetch(url, options);
+    if (response.ok) {
+      return response;
+    }
+    else {
+      throw new Error(`${response.status} (${response.statusText})`);
+    }
+  }
+  catch (error) {
+    throw new Error('Error in making HTTP request: ' + error.toString()); // toString method prepends also the response.name to the message
+  }
+}
+
+export const loadCities = async () => {
+  const response = await fetch("data/cities.json");
+  const cities = await response.json();
+  return cities;
+}
+
+export const mapWmoToTheme = code => {
+  if (code <= 3) return 'sunny';
+  if (code <= 48) return 'cloudy';
+  if (code <= 67) return 'rainy';
+  return 'snowy';
+}
+
+export const getWmoDescription = code => {
+  const codes = { 0: "Clear Sky", 1: "Mainly Clear", 61: "Slight Rain" }; // Add more as needed
+  return codes[code] || "Variable Conditions";
+}
